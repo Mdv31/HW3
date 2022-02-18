e117a1b1668761e890b2294ba55922b10daee05c
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-18 23:52:01 +0300

    Add files via upload

--- src/main/java/animals/Animal.java
+++ src/main/java/animals/Animal.java
@@ -3,7 +3,7 @@ package animals;
 import food.Food;
 
 public abstract class Animal {
-    private int satiety = 10;
+    private int satiety;
 
     public int getSatiety() {
         //return satiety+=10;
