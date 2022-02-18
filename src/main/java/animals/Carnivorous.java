79e069b419db5d4ca4dc5d89185ba72b212b97fe
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-18 23:37:52 +0300

    Add files via upload

--- src/main/java/animals/Carnivorous.java
+++ src/main/java/animals/Carnivorous.java
@@ -4,12 +4,12 @@ import food.Food;
 import food.Meat;
 
 public abstract class Carnivorous extends Animal {
-
+    int satiety;
     @Override
     public void eat(Food food) {
         if (food instanceof Meat) {
             System.out.println("Хищники едят мясо");
-//            this.getSatiety(((Meat) food).getEnergy());
+            this.satiety = this.getSatiety()+ food.getEnergy();
         } else System.out.println("Хищники не едят траву");
 
     }
--- src/main/java/animals/Herbivore.java
+++ src/main/java/animals/Herbivore.java
@@ -4,10 +4,12 @@ import food.Food;
 import food.Grass;
 
 public abstract class Herbivore extends Animal {
+    int satiety;
     @Override
     public void eat(Food food) {
         if (food instanceof Grass) {
             System.out.println("Травоядные едят траву");
+            this.satiety = this.getSatiety()+ food.getEnergy();
         } else System.out.println("Травоядные не едят мясо");
 
     }
