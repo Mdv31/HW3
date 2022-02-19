f204a7bbc5b76a595c3ac2bceb85aa5512dd5481
Mdv31 <99442332+Mdv31@users.noreply.github.com>
2022-02-19 11:22:33 +0300

    + функции адд, гет,ремув анимал в класск Авиари

--- src/main/java/model/Aviary.java
+++ src/main/java/model/Aviary.java
@@ -1,10 +1,25 @@
 package model;
 
+import animals.Duck;
+
+import java.util.HashMap;
+
+
 public class Aviary <Animal> {
     private Size size;
 
     public Aviary(Size size) {
 
     }
+    HashMap<String ,Animal> namesAndAnimals = new HashMap<>();
 
+    public void addAnimal(String name, Animal animal){
+        namesAndAnimals.put(name, animal);
+    }
+    public void getAnimal (String name){
+        namesAndAnimals.get(name);
+    }
+    public void removeAnimal (String name){
+        namesAndAnimals.remove(name);
+    }
 }
